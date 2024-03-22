4ae06d58352ba1ee7af7dfaa26a0767527c914d9
Sivram Pradhan <programmersiv21@gmail.com>
2024-03-22 19:43:08 +0530

    changes

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,164 @@
 import React from 'react'
+import image1 from '../assets/image1.avif'
+import logo from "../assets/logo-no-background.png"
 
 const Dashboard = () => {
   return (
-    <div className='flex justify-center items-center text-white text-3xl h-full'>
+    <div className='bg-richblack-900 '>
+      <div className='flex text-white text-3xl ml-[135px] mr-[135px]'>
+        <div className='mt-10'>
+          <h1 className='text-richblack-200'>New and noteworthy</h1>
+          <div className='flex flex-row gap-5 '>
+            <div>
+              <img src={image1} className='w-auto h-[230px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-auto h-[230px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-auto h-[230px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-auto h-[230px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex text-white text-3xl ml-[135px] mr-[135px]'>
+        <div className='mt-10'>
+          <h1 className='text-richblack-200'>Most booked services</h1>
+          <div className='flex flex-row gap-5 '>
+            <div>
+              <img src={image1} className='w-auto h-[230px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-auto h-[230px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-auto h-[230px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-auto h-[230px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='flex text-white text-3xl ml-[135px] mr-[135px]'>
+        <div className='mt-10'>
+          <h1 className='text-richblack-200'>Salon for women</h1>
+          <div className='flex flex-row gap-5 '>
+            <div>
+              <img src={image1} className='w-[274px] h-[215px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-[274px] h-[215px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-[274px] h-[215px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+      <div className='flex text-white text-3xl ml-[135px] mr-[135px]'>
+        <div className='mt-10'>
+          <h1 className='text-richblack-200'>Quick home repairs</h1>
+          <div className='flex flex-row gap-5 '>
+            <div>
+              <img src={image1} className='w-auto h-[230px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-auto h-[230px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-auto h-[230px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-auto h-[230px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex text-white text-3xl ml-[135px] mr-[135px]'>
+        <div className='mt-10'>
+          <h1 className='text-richblack-200'>AC & appliance repair</h1>
+          <div className='flex flex-row gap-5 '>
+          <div>
+              <img src={image1} className='w-[274px] h-[215px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-[274px] h-[215px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex text-white text-3xl ml-[135px] mr-[135px]'>
+        <div className='mt-10'>
+          <h1 className='text-richblack-200'>Message for Men</h1>
+          <div className='flex flex-row gap-5 '>
+          <div>
+              <img src={image1} className='w-[274px] h-[215px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-[274px] h-[215px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-[274px] h-[215px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex text-white text-3xl ml-[135px] mr-[135px] mb-[100px]'>
+        <div className='mt-10'>
+          <h1 className='text-richblack-200'>Salon for kids & men</h1>
+          <div className='flex flex-row gap-5 '>
+          <div>
+              <img src={image1} className='w-[274px] h-[215px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-[274px] h-[215px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+            <div>
+              <img src={image1} className='w-[274px] h-[215px] mt-10 rounded-lg hover:scale-105 cursor-pointer' />
+              <h1 className='text-richblack-100 text-xl mt-5 '>Native Smart Locks</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <footer className='bg-richblack-200 w-full h-[500px]'>
+      <div className=''>
+      <img src={logo} alt="Logo" width={170} height={15} loading="lazy" className='ml-[135px] mt-10'/>
+      </div>
+    </footer>
     </div>
+
   )
 }
 
