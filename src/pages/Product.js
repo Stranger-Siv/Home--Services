3dbc019be05a70749ab26ffcc1ad9d27c28e54e4
Sivram Pradhan <programmersiv21@gmail.com>
2024-03-24 02:38:51 +0530

    changes

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Footer from "../components/Footer";
 
 const Product = () => {
     return(
         <div>
-            
+            <h1 className="text-white text-center text-2xl">Welcome to product Page</h1>
+            <div className="h-full">
+
+            </div>
+            <Footer />
         </div>
+        
     )
 }
 
