56b4af9b0aca45f02208ea9ba8361d9858fc4df6
Sivram Pradhan <programmersiv21@gmail.com>
2024-03-25 05:54:19 +0530

    changes

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Footer from "../components/Footer";
-import ProductTemp from "../components/ProductTemp";
+import image from "../assets/ar2.jpeg"
 
-const Product = (title,image) => {
-    return(
-         <div>
-            <h1 className="text-white text-center text-2xl mb-[700px]">Welcome</h1>
-            <Footer />
+
+const Product = () => {
+    return (
+        <div className="relative  bg-richblack-900">
+            <div className="flex flex-row gap-[120px] mt-10 mr-[135px]">
+                <div className="flex flex-col h-[1200px]">
+                    <h1 className="text-white ml-[135px] text-4xl mb-[700px] ">Smart Locks</h1>
+                    <button className="border-2 bg-white text-md w-[140px] py-[7px] px-[9px] ml-[135px] mt-[80px] rounded-lg absolute">View Service</button>
+                </div>
+                <img src={image} className="w-[850px] h-[550px] rounded-lg" />
+            </div>
+            <div className=" text-white rounded-lg">
+                <h1 className="text-white">Full Home Painting</h1>
+            </div>
+
+            <Footer/>
         </div>
-        
+
     )
 }
 
